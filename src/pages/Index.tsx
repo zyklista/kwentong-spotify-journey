8754a76f01a2380bae8ba62c1a76d3e51915c091
zyklista <80622109+zyklista@users.noreply.github.com>
2025-09-18 08:50:19 +0200

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,7 +2,6 @@
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import MediaSection from "@/components/MediaSection";
-import YouTubeSection from "@/components/YouTubeSection";
 import ContactForm from "@/components/ContactForm";
 import ReviewsSection from "@/components/ReviewsSection";
 import Footer from "@/components/Footer";
@@ -16,7 +15,6 @@ const Index = () => {
       <main>
         <Hero />
         <MediaSection />
-        <YouTubeSection />
         <ContactForm />
         <ReviewsSection />
       </main>
