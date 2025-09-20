6150ba647d0ff4fa137e04f1cb472578682f81fb
zyklista <80622109+zyklista@users.noreply.github.com>
2025-09-20 17:47:33 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    // You can add other plugins here if needed
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [react()],
+  // other config options
 });
